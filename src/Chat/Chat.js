ae1d5110e846926824c38320157e82ec760a69bc
HXgrowns <804156017@qq.com>
2020-09-23 12:00:05 +0800

    [huxiao]: refactor chat.js code

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -5,6 +5,7 @@ import ChatBox from './ChatBox/ChatBox';
 import ChatInput from './ChatInput/ChatInput';
 import shopData from '../data/shop.json';
 import answersData from '../data/answers.json';
+import { ROLE } from '../constants';
 
 class Chat extends Component {
   constructor(props, context) {
@@ -27,35 +28,51 @@ class Chat extends Component {
     }, 1000);
   }
 
-  handleChatMessage = (text)  => {
-
-    const customerMessage = {
-      text: [text],
-      "role": "CUSTOMER",
-    }
-
-    setTimeout(() => {
-      const messages = this.state.messages.concat(customerMessage);
-      this.setState({
-        messages,
+  handlerobotMessage = (text) => {
+    let robotMessage = answersData.find((answer) => {
+      answer.tags.forEach(tag => {
+        const regex = new RegExp(tag);
+        if (regex.test(text)) {
+          return regex.exec(text);
+        }
       });
-    }, 500);
-
-    const chatMessage = answersData.find((answer) => {
-      const regex = new RegExp(answer.tags)
-      return regex.exec(text);
+      
+      // const regex = new RegExp(answer.tags);
+      // return regex.exec(text);
     });
 
-    if (chatMessage === undefined) {
-      return;
+    if (robotMessage === undefined) {
+      robotMessage = {
+        role:ROLE.ROBOT,
+        text:"对不起，我没有理解你的问题，需要联系人工吗？电话：888888",
+      }
     }
-  
-    const messages = this.state.messages.concat(customerMessage).concat(chatMessage);
+
+    return robotMessage;
+  }
+
+  handlerCustomerMessage = (text) => {
+    const customerMessage = {
+      text: [text],
+      role: ROLE.CUSTOMER,
+    }
+    return customerMessage;
+  }
+
+  handleSetState = (message, time) => {
     setTimeout(() => {
+      const newMessages = this.state.messages.concat(message);
       this.setState({
-        messages,
+        messages: newMessages,
       });
-    }, 1000);
+    }, time);
+  }
+
+  handleChatMessage = (text) => {
+    const customerMessage = this.handlerCustomerMessage(text);
+    const robotMessage = this.handlerobotMessage(text);
+    this.handleSetState(customerMessage, 500);
+    this.handleSetState(robotMessage, 1000);
   }
 
   render() {
@@ -64,7 +81,7 @@ class Chat extends Component {
       <main className="Chat">
         <ChatHeader shop={shop} />
         <ChatBox messages={messages} />
-        <ChatInput handleChatMessage={this.handleChatMessage}/>
+        <ChatInput handleChatMessage={this.handleChatMessage} />
       </main>
     );
   }
